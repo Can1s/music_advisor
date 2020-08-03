e7d20647401adfedd4f83002b99d4d0c9c1c8a70
Erdaulet <47812867+Can1s@users.noreply.github.com>
2020-08-03 18:40:32 +0600

    Stage 2/5
    Complete second stage

--- Main.java
+++ Main.java
@@ -5,42 +5,53 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean auth = false;
         boolean tr = true;
+        String CLIENT_ID = "j4ll351l51h5515j1l51jnflsfs";
         while (tr) {
             String action = scanner.nextLine();
-            switch (action) {
-                case "new":
-                    System.out.println("---NEW RELEASES---\n" +
-                            "Mountains [Sia, Diplo, Labrinth]\n" +
-                            "Runaway [Lil Peep]\n" +
-                            "The Greatest Show [Panic! At The Disco]\n" +
-                            "All Out Life [Slipknot]");
-                    break;
-                case "featured":
-                    System.out.println("---FEATURED---\n" +
-                            "Mellow Morning\n" +
-                            "Wake Up and Smell the Coffee\n" +
-                            "Monday Motivation\n" +
-                            "Songs to Sing in the Shower");
-                    break;
-                case "categories":
-                    System.out.println("---CATEGORIES---\n" +
-                            "Top Lists\n" +
-                            "Pop\n" +
-                            "Mood\n" +
-                            "Latin");
-                    break;
-                case "playlists Mood":
-                    System.out.println("---MOOD PLAYLISTS---\n" +
-                            "Walk Like A Badass  \n" +
-                            "Rage Beats  \n" +
-                            "Arab Mood Booster  \n" +
-                            "Sunday Stroll");
-                    break;
-                case "exit":
-                    System.out.println("---GOODBYE!---");
-                    tr = false;
-                    break;
+            if (action.equals("exit")) {
+                System.out.println("---GOODBYE!---");
+                tr = false;
+            } else if (action.equals("auth") && (!auth)) {
+                auth = true;
+                System.out.println("https://accounts.spotify.com/authorize?" +
+                        "client_id=" + CLIENT_ID +
+                        "redirect_uri=http://localhost:8080&response_type=code\n" +
+                        "---SUCCESS---");
+            } else if (auth && !(action.equals("auth"))) {
+                switch (action) {
+                    case "new":
+                        System.out.println("---NEW RELEASES---\n" +
+                                "Mountains [Sia, Diplo, Labrinth]\n" +
+                                "Runaway [Lil Peep]\n" +
+                                "The Greatest Show [Panic! At The Disco]\n" +
+                                "All Out Life [Slipknot]");
+                        break;
+                    case "featured":
+                        System.out.println("---FEATURED---\n" +
+                                "Mellow Morning\n" +
+                                "Wake Up and Smell the Coffee\n" +
+                                "Monday Motivation\n" +
+                                "Songs to Sing in the Shower");
+                        break;
+                    case "categories":
+                        System.out.println("---CATEGORIES---\n" +
+                                "Top Lists\n" +
+                                "Pop\n" +
+                                "Mood\n" +
+                                "Latin");
+                        break;
+                    case "playlists Mood":
+                        System.out.println("---MOOD PLAYLISTS---\n" +
+                                "Walk Like A Badass  \n" +
+                                "Rage Beats  \n" +
+                                "Arab Mood Booster  \n" +
+                                "Sunday Stroll");
+                        break;
+                }
+            } else {
+                System.out.println("Please, provide access for application.");
             }
         }
     }
